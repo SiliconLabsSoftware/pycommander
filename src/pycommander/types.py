28431287b8164050970ac83b96459ad30c2a266a
Espen Hovland <eshovlan@silabs.com>
2026-02-09 16:10:17 +0100

    Add basic info methods for Adapter and Device

--- src/pycommander/types.py
+++ src/pycommander/types.py
@@ -1,7 +1,45 @@
 from dataclasses import dataclass
 
 @dataclass
+class AdapterBoardInfo:
+  name:          str | None = None
+  part_number:   str | None = None
+  serial_number: str | None = None
+  target_device: str | None = None
+
+@dataclass
+class AdapterFwInfo:
+  current_version:   str  | None = None
+  latest_version:    str  | None = None
+  upgrade_available: bool | None = None
+
+@dataclass
+class AdapterInfo:
+  board_list:          list[AdapterBoardInfo] | None = None
+  fw_info:             AdapterFwInfo          | None = None
+  jlink_serial_number: int                    | None = None
+  vcom_port:           str                    | None = None
+  vcom_supported:      bool                   | None = None
+  ip_supported:        bool                   | None = None
+  ip_address:          str                    | None = None
+  mac_address:         str                    | None = None
+  kit_name:            str                    | None = None
+  kit_part_number:     str                    | None = None
+  aem_supported:       bool                   | None = None
+  debug_mode:          str                    | None = None
+  debug_part:          str                    | None = None
+
+@dataclass
 class CtuneValue:
   di:    int | None = None # Value from the Device Info page
   board: int | None = None # Value from the EEPROM on the board
   token: int | None = None # Value from the MFG token
+
+@dataclass
+class DeviceInfo:
+  part_number:        str | None = None
+  die_revision:       str | None = None
+  production_version: str | None = None
+  flash_size_kb:      int | None = None
+  sram_size_kb:       int | None = None
+  unique_id:          str | None = None
