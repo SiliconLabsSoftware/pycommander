6239d38ebc4dec67fe9b7220520a608a1838db59
Espen Hovland <eshovlan@silabs.com>
2026-02-10 15:28:05 +0100

    Error out properly if no version of pycommander was specified

--- packages/pycommander/src/pycommander/__init__.py
+++ packages/pycommander/src/pycommander/__init__.py
@@ -1,7 +1,15 @@
 try:
   from pycommander_gui import Commander, Adapter, Device
 except ImportError:
-  from pycommander_cli import Commander, Adapter, Device
+  try:
+    from pycommander_cli import Commander, Adapter, Device
+  except ImportError:
+    raise ImportError(
+      "No version of Simplicity Commander is installed.\n\n"
+      "Install one of:\n"
+      "  pip install silabs-pycommander[cli]\n"
+      "  pip install silabs-pycommander[gui]\n"
+    )
 
 __all__ = [
   "Commander",
