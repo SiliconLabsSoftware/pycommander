204daa6abc15ef66f736254ee40aec70cafc0798
Espen Hovland <eshovlan@silabs.com>
2026-02-12 11:18:21 +0100

    Add unittests for Device class
    Will have to add stuff here when we add new methods

---
+++ packages/pycommander-core/tests/test_device.py
@@ -0,0 +1,432 @@
+import unittest
+
+from pycommander_core.device import Device
+from pycommander_core.runner import RunnerResult
+from pycommander_core.types import DeviceInfo, CtuneValue
+
+from tests.mock_commander import MockCommander
+
+class TestDevice(unittest.TestCase):
+  
+  def test_device_info(self):
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "result": {
+        "device_info": {
+            "die_revision": "A0",
+            "flash_size_kb": 1536,
+            "part_number": "EFR32MG24B020F1536IM48",
+            "production_version": "0",
+            "sram_size_kb": 256,
+            "unique_id": "84fd27fffe64ac04"
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    expected_device_info = DeviceInfo(
+      part_number="EFR32MG24B020F1536IM48",
+      die_revision="A0",
+      production_version="0",
+      flash_size_kb=1536,
+      sram_size_kb=256,
+      unique_id="84fd27fffe64ac04",
+    )
+
+    self.assertEqual(device.info(), expected_device_info)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "device", "info", "--serialno", "123456789", "--json"]])
+
+  def test_device_info_failed(self):
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    device._commander._runner.queue_result(RunnerResult(254, '{"success": false, "error": "Failed to get device information"}'))
+    
+    self.assertEqual(device.info(), None)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "device", "info", "--serialno", "123456789", "--json"]])
+
+  def test_device_getCTUNE(self):
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 103
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    expected_ctune_value = CtuneValue(
+      di=None,
+      board=103,
+      token=92,
+    )
+
+    self.assertEqual(device.getCTUNE(), expected_ctune_value)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]])
+
+  def test_device_getCTUNE_failed(self):
+    """
+    Test the device getCTUNE method when getting the CTUNE value from the board fails.
+    The method should return None.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    device._commander._runner.queue_result(RunnerResult(254, '{"success": false, "error": "Failed to get CTUNE value from the board"}'))
+    
+    self.assertEqual(device.getCTUNE(), None)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]])
+
+  def test_device_setCTUNE_autoset_same_value(self):
+    """
+    Test the device setCTUNE method with autoset, no force, and the desired value is the same as the current value in the board EEPROM.
+    The method should return True and *not* call the autoset command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    self.assertEqual(device.setCTUNE(), True)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]])
+
+  def test_device_setCTUNE_autoset_same_value_force(self):
+    """
+    Test the device setCTUNE method with autoset, force, and the desired value is the same as the current value in the board EEPROM.
+    The method should return True and call the autoset command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    # Force was specified, so we call the autoset command. The result from this should just be true, no more fuss than that.
+    device._commander._runner.queue_result(RunnerResult(0, '{"success": true}'))
+
+    self.assertEqual(device.setCTUNE(force=True), True)
+    self.assertEqual(commander._runner.logged_commands,
+    [
+      ["mock", "ctune", "get",     "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"],
+      ["mock", "ctune", "autoset", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]
+    ])
+
+  def test_device_setCTUNE_autoset_different_value(self):
+    """
+    Test the device setCTUNE method with autoset, no force, and the desired value is different from the current value in the board EEPROM.
+    The method should return True and call the autoset command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 103
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+    
+    # Then we call the autoset command. The result from this should just be true, no more fuss than that.
+    device._commander._runner.queue_result(RunnerResult(0, '{"success": true}'))
+
+    self.assertEqual(device.setCTUNE(), True)
+    self.assertEqual(commander._runner.logged_commands,
+    [
+      ["mock", "ctune", "get",     "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"],
+      ["mock", "ctune", "autoset", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]
+    ])
+
+  def test_device_setCTUNE_set_same_value(self):
+    """
+    Test the device setCTUNE method with set, no force, and the desired value is the same as the current value in the board EEPROM.
+    The method should return True and *not* call the set command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    self.assertEqual(device.setCTUNE(value=92, force=False), True)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]])
+
+  def test_device_setCTUNE_set_same_value_force(self):
+    """
+    Test the device setCTUNE method with set, force, and the desired value is the same as the current value in the board EEPROM.
+    The method should return True and call the set command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+    
+    # Force was specified, so we call the set command. The result from this should just be true, no more fuss than that.
+    device._commander._runner.queue_result(RunnerResult(0, '{"success": true}'))
+
+    self.assertEqual(device.setCTUNE(value=92, force=True), True)
+    self.assertEqual(commander._runner.logged_commands,
+    [
+      ["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"],
+      ["mock", "ctune", "set", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--value", "0x0000005C", "--json"]
+    ])
+
+  def test_device_setCTUNE_set_different_value(self):
+    """
+    Test the device setCTUNE method with set, no force, and the desired value is different from the current value in the board EEPROM.
+    The method should return True and call the set command.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    # We get the CTUNE value from the board first
+    device._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "info": [
+        "Getting CTUNE values from the Device Info page, stored in EEPROM on the board, and MFG token."
+    ],
+    "result": {
+        "ctune": {
+            "board": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            },
+            "di": {
+                "status_str": "Not set",
+                "valid": false,
+                "value": 0
+            },
+            "token": {
+                "status_str": "OK",
+                "valid": true,
+                "value": 92
+            }
+        }
+    },
+    "success": true
+}
+"""
+      )
+    )
+    
+    device._commander._runner.queue_result(RunnerResult(0, '{"success": true}'))
+
+    self.assertEqual(device.setCTUNE(value=93, force=False), True)
+    self.assertEqual(commander._runner.logged_commands,
+    [
+      ["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"],
+      ["mock", "ctune", "set", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--value", "0x0000005D", "--json"]
+    ])
+
+  def test_device_setCTUNE_get_failed(self):
+    """
+    Test the device setCTUNE method when getting the CTUNE value from the board fails.
+    The method should return False.
+    """
+
+    commander = MockCommander(serial_number="123456789")
+    device = Device(part_number="EFR32MG24B020F1536IM48", commander=commander)
+    
+    device._commander._runner.queue_result(RunnerResult(254, '{"success": false, "error": "Failed to get CTUNE value from the board"}'))
+    
+    self.assertEqual(device.setCTUNE(), False)
+    self.assertEqual(commander._runner.logged_commands, [["mock", "ctune", "get", "--serialno", "123456789", "--device", "EFR32MG24B020F1536IM48", "--json"]])
