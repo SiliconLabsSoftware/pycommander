51468869343e204861257fce0d725df14afcbe9d
Espen Hovland <eshovlan@silabs.com>
2026-02-12 18:17:54 +0100

    Fix CLI handling

--- packages/pycommander-cli/src/pycommander_cli/cli.py
+++ packages/pycommander-cli/src/pycommander_cli/cli.py
@@ -4,7 +4,7 @@ from pycommander_core.cli import PyCommanderCLI
 from pycommander_core.paths import EXECUTABLE_PATH_CLI
 
 def main() -> int:
-  cli = PyCommanderCLI(EXECUTABLE_PATH_CLI)
+  cli = PyCommanderCLI(cli=True, executable_path=EXECUTABLE_PATH_CLI)
   args = sys.argv[1:]  # Skip the script name
   return cli.run(*args)
 
--- packages/pycommander-core/src/pycommander_core/cli.py
+++ packages/pycommander-core/src/pycommander_core/cli.py
@@ -1,18 +1,16 @@
-import sys
 import subprocess
 
 from pathlib import Path
 
-from .paths import EXECUTABLE_PATH_CLI
 from ._ensure_commander import ensure_commander
 from ._version import __version__
 
 class PyCommanderCLI:
-  def __init__(self, executable_path: Path):
+  def __init__(self, cli: bool, executable_path: Path):
     self.executable_path = executable_path
     self.version = __version__
 
-    ensure_commander()
+    ensure_commander(cli=cli)
 
   def run(self, *args: str) -> int:
     if "-v" in args or "--version" in args:
@@ -23,13 +21,3 @@ class PyCommanderCLI:
       return result.returncode
     except KeyboardInterrupt:
       return 128 + 2 # 128 + SIGINT
-
-
-def main() -> int:
-  cli = PyCommanderCLI(EXECUTABLE_PATH_CLI)
-  args = sys.argv[1:]  # Skip the script name
-  return cli.run(*args)
-
-
-if __name__ == "__main__":
-  raise SystemExit(main())
