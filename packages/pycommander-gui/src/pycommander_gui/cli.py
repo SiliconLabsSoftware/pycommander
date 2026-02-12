88e2cd299aa1d5be1a1ccaf91e74800b01ff8546
Espen Hovland <eshovlan@silabs.com>
2026-02-12 18:17:54 +0100

    Fix CLI handling

--- packages/pycommander-gui/src/pycommander_gui/cli.py
+++ packages/pycommander-gui/src/pycommander_gui/cli.py
@@ -4,7 +4,7 @@ from pycommander_core.cli import PyCommanderCLI
 from pycommander_core.paths import EXECUTABLE_PATH_GUI
 
 def main() -> int:
-  cli = PyCommanderCLI(EXECUTABLE_PATH_GUI)
+  cli = PyCommanderCLI(cli=False, executable_path=EXECUTABLE_PATH_GUI)
   args = sys.argv[1:]  # Skip the script name
   return cli.run(*args)
 
