b0d972d5ead48c714d886d1f63f720521249eaf8
Espen Hovland <eshovlan@silabs.com>
2026-02-12 14:54:14 +0100

    ... but don't commit that flash method just yet

--- packages/pycommander-core/src/pycommander_core/device.py
+++ packages/pycommander-core/src/pycommander_core/device.py
@@ -54,31 +54,6 @@ class Device:
     result = self._commander.device.masserase(device=self.part_number)
     return result["success"]
 
-  def flash(self, filenames: list[Path], address: int | None = None, treat_as_binary: bool = False, force: bool = False) -> bool:
-    """Flash a binary file (.bin, .s37, .hex, .gbl, or .rps) to the device.
-
-    Args:
-      filenames (list[Path]): The paths to the binary files to flash.
-      address (int): The address to flash the binary file to. If the file is a .hex or .s37 file, the address will be ignored.
-      treat_as_binary (bool): Treat the file as a flat binary file, regardless of the file extension.
-      force (bool): Whether to force the flash.
-
-    Returns:
-      True if the flashing was successful, False otherwise.
-    """
-
-    for filename in filenames:
-      if not filename.exists():
-        raise FileNotFoundError(f"File {filename} does not exist")
-
-    result = self._commander.flash.flash(
-      filenames=[str(filename) for filename in filenames],
-      target_device=self.part_number,
-      force=force
-    )
-
-    return result["success"]
-
 
   def getCTUNE(self) -> CtuneValue | None:
     """Get the CTUNE values from the DI, board and token on the device.
