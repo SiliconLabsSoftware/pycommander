e5cee024b376d972be6736389db5f6c8271e1665
Espen Hovland <eshovlan@silabs.com>
2026-02-19 13:11:28 +0100

    Bump up test coverage

--- packages/pycommander-core/tests/test_adapter_base.py
+++ packages/pycommander-core/tests/test_adapter_base.py
@@ -1,11 +1,30 @@
 import unittest
 
+from pycommander_core.device import Device
+from pycommander_core.adapter_base import AdapterBase
 from pycommander_core.runner import RunnerResult
-from pycommander_core.types import AdapterInfo, AdapterBoardInfo, AdapterFwInfo
+from pycommander_core.types import AdapterInfo, AdapterBoardInfo, AdapterFwInfo, AdapterVoltageInfo
 
 from .mock_adapter import MockAdapter
+from .mock_commander import MockCommander
 
 class TestAdapterBase(unittest.TestCase):
+  def test_adapter_base_init(self):
+    mock_commander = MockCommander(serial_number="123456789")
+    mock_device = Device(part_number="EFR32MG24B020F1536IM48", commander=mock_commander)
+    adapter = AdapterBase(commander=mock_commander, target=mock_device)
+
+    self.assertEqual(adapter._commander, mock_commander)
+    self.assertEqual(adapter.target, mock_device)
+
+  def test_adapter_base_init_failed(self):
+    with self.assertRaises(ValueError):
+      AdapterBase(commander=None, target=None)
+    with self.assertRaises(ValueError):
+      AdapterBase(commander=MockCommander(serial_number="123456789"), target=None)
+    with self.assertRaises(ValueError):
+      AdapterBase(commander=None, target=Device(part_number="EFR32MG24B020F1536IM48", commander=MockCommander(serial_number="123456789")))
+
   def test_adapter_base_info(self):
     adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
     
@@ -219,3 +238,198 @@ class TestAdapterBase(unittest.TestCase):
     
     self.assertEqual(adapter.info(), None)
     self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "probe", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_reset(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "success": true
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.reset(), True)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "reset", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_reset_failed(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        254,
+"""
+{
+    "success": false
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.reset(), False)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "reset", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_getVoltage(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "result": {
+        "voltages": [
+            {
+                "configured_voltage_v": 3.299999952316284,
+                "measured_voltage_v": 3.308469772338867,
+                "rail_index": 0,
+                "rail_powered": true
+            }
+        ]
+    },
+    "success": true
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.getVoltage(), {0: AdapterVoltageInfo(
+      configured_voltage_v=3.299999952316284,
+      measured_voltage_v=3.308469772338867,
+      rail_powered=True,
+    )})
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "voltage", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_getVoltage_multiple_rails(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "result": {
+        "voltages": [
+            {
+                "configured_voltage_v": 3.299999952316284,
+                "measured_voltage_v": 3.308469772338867,
+                "rail_index": 0,
+                "rail_powered": true
+            },
+            {
+                "configured_voltage_v": 1.8,
+                "measured_voltage_v": 1.8,
+                "rail_index": 1,
+                "rail_powered": false
+            }
+        ]
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    expected_voltage_info_dict : dict[int, AdapterVoltageInfo] = {
+      0: AdapterVoltageInfo(
+        configured_voltage_v=3.299999952316284,
+        measured_voltage_v=3.308469772338867,
+        rail_powered=True,
+      ),
+      1: AdapterVoltageInfo(
+        configured_voltage_v=1.8,
+        measured_voltage_v=1.8,
+        rail_powered=False,
+      ),
+    }
+
+    self.assertEqual(adapter.getVoltage(), expected_voltage_info_dict)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "voltage", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_getVoltage_missing_one_rail_index(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "result": {
+        "voltages": [
+            {
+                "configured_voltage_v": 3.299999952316284,
+                "measured_voltage_v": 3.308469772338867,
+                "rail_index": 0,
+                "rail_powered": true
+            },
+            {
+                "configured_voltage_v": 1.8,
+                "measured_voltage_v": 1.8,
+                "rail_powered": false
+            }
+        ]
+    },
+    "success": true
+}
+"""
+      )
+    )
+
+    expected_voltage_info_dict : dict[int, AdapterVoltageInfo] = {
+      0: AdapterVoltageInfo(
+        configured_voltage_v=3.299999952316284,
+        measured_voltage_v=3.308469772338867,
+        rail_powered=True,
+      ),
+    }
+
+    self.assertEqual(adapter.getVoltage(), expected_voltage_info_dict)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "voltage", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_getVoltage_failed(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        254,
+"""
+{
+    "success": false
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.getVoltage(), None)
+
+  def test_adapter_base_setVoltage(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        0,
+"""
+{
+    "success": true
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.setVoltage(3.3), True)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "voltage", "3.3", "--serialno", "123456789", "--json"]])
+
+  def test_adapter_base_setVoltage_failed(self):
+    adapter = MockAdapter(serial_number="123456789", target_device="EFR32MG24B020F1536IM48")
+    
+    adapter._commander._runner.queue_result(
+      RunnerResult(
+        254,
+"""
+{
+    "success": false
+}
+"""
+      )
+    )
+    self.assertEqual(adapter.setVoltage(3.3), False)
+    self.assertEqual(adapter._commander._runner.logged_commands, [["mock", "adapter", "voltage", "3.3", "--serialno", "123456789", "--json"]])
---
+++ packages/pycommander-gui/tests/test_adapter.py
@@ -0,0 +1,49 @@
+import unittest
+
+from pathlib import Path
+
+from pycommander_gui.adapter import Adapter
+from pycommander_gui.commander import Commander
+from pycommander_core.device import Device
+
+class TestAdapter(unittest.TestCase):
+  def test_adapter_no_commander_missing_connection_arguments(self):
+    with self.assertRaisesRegex(ValueError, "Either serial_number, ip_address, or serial_port must be provided"):
+      Adapter(target_device="EFR32MG24", debug_speed=1000, debug_tif="SWD", debug_irpre=1, debug_drpre=1)
+
+  def test_adapter_no_commander_with_serial_number(self):
+    adapter = Adapter(serial_number="123456789", target_device="EFR32MG24", debug_speed=1000, debug_tif="SWD", debug_irpre=1, debug_drpre=1)
+    self.assertEqual(adapter._commander._serial_number, "123456789")
+    self.assertEqual(adapter._commander._debug_speed, 1000)
+    self.assertEqual(adapter._commander._debug_tif, "SWD")
+    self.assertEqual(adapter._commander._debug_irpre, 1)
+    self.assertEqual(adapter._commander._debug_drpre, 1)
+
+  def test_adapter_no_commander_with_ip_address(self):
+    adapter = Adapter(ip_address="192.168.1.100", target_device="EFR32MG24", debug_speed=1000, debug_tif="SWD", debug_irpre=1, debug_drpre=1)
+    self.assertEqual(adapter._commander._ip_address, "192.168.1.100")
+    self.assertEqual(adapter._commander._debug_speed, 1000)
+    self.assertEqual(adapter._commander._debug_tif, "SWD")
+    self.assertEqual(adapter._commander._debug_irpre, 1)
+    self.assertEqual(adapter._commander._debug_drpre, 1)
+
+  def test_adapter_no_commander_with_serial_port(self):
+    adapter = Adapter(serial_port="/dev/tty.usbmodem141101", target_device="EFR32MG24", debug_speed=1000, debug_tif="SWD", debug_irpre=1, debug_drpre=1)
+    self.assertEqual(adapter._commander._serial_port, "/dev/tty.usbmodem141101")
+    self.assertEqual(adapter._commander._debug_speed, 1000)
+    self.assertEqual(adapter._commander._debug_tif, "SWD")
+    self.assertEqual(adapter._commander._debug_irpre, 1)
+    self.assertEqual(adapter._commander._debug_drpre, 1)
+
+  def test_adapter_yes_commander_missing_target_device(self):
+    commander = Commander(serial_number="123456789", executable_path=Path("mock"))
+
+    with self.assertRaisesRegex(ValueError, "target_device must be provided"):
+      Adapter(commander=commander)
+
+  def test_adapter_yes_commander_and_target_device(self):
+    commander = Commander(serial_number="123456789", executable_path=Path("mock"))
+    adapter = Adapter(commander=commander, target_device="EFR32MG24")
+
+    self.assertEqual(adapter._commander, commander)
+    self.assertEqual(adapter.target, Device(part_number="EFR32MG24", commander=commander))
