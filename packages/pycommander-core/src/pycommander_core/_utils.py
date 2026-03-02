17d556a4992df1880e09ba45c28796ed01859c09
Espen Hovland <eshovlan@silabs.com>
2026-03-02 15:19:31 +0100

    Clean up args a little better

--- packages/pycommander-core/src/pycommander_core/_utils.py
+++ packages/pycommander-core/src/pycommander_core/_utils.py
@@ -1,9 +1,18 @@
 def sanitize_args(args: list[str]) -> list[str]:
+  # NOTE: We do NOT want to split any args that contain whitespace into multiple args.
+  #       There might be filenames or other arguments that have whitespace in them.
+
   sanitized_args : list[str] = []
   for arg in args:
-    # Strip away any empty or None elements
+    # Remove any empty or None elements
     if not arg:
       continue
 
-    sanitized_args.append(arg)
+    # Remove any whitespace-only elements
+    if arg.strip() == "":
+      continue
+
+    # Trim leading and trailing whitespace
+    sanitized_args.append(arg.strip())
+
   return sanitized_args
