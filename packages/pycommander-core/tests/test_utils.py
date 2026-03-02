e329071c90b269a57ac3974ebf2f85524538379b
Espen Hovland <eshovlan@silabs.com>
2026-03-02 15:19:31 +0100

    Clean up args a little better

--- packages/pycommander-core/tests/test_utils.py
+++ packages/pycommander-core/tests/test_utils.py
@@ -6,7 +6,7 @@ class TestUtils(unittest.TestCase):
   def test_sanitize_args(self):
     self.assertEqual(sanitize_args(["command", "arg1", "arg2"]), ["command", "arg1", "arg2"])
     self.assertEqual(sanitize_args(["command", "arg 1", "arg 2"]), ["command", "arg 1", "arg 2"])
-    self.assertEqual(sanitize_args(["command", "", "arg2"]), ["command", "arg2"])
+    self.assertEqual(sanitize_args(["command", "", " ", " arg2 "]), ["command", "arg2"])
     self.assertEqual(sanitize_args(["command", None, "arg3"]), ["command", "arg3"])
     self.assertEqual(sanitize_args(["command", "arg1", None, "arg3"]), ["command", "arg1", "arg3"])
     self.assertEqual(sanitize_args(["command", "arg1", "arg2", "arg3"]), ["command", "arg1", "arg2", "arg3"])
